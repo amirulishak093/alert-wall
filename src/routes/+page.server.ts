b2cb6f7f09c6f6857616b0cfe31ebcd7ba463833
Amirul Ishak <70368867+amirulishak093@users.noreply.github.com>
2023-11-03 09:22:57 +0800

    Update +page.server.ts

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -3,16 +3,24 @@ import type { Actions } from "@sveltejs/kit";
 
 
 export async function load() {
-  const today = new Date();
-  today.setHours(0, 0, 0, 0); 
-  const tomorrow = new Date(today);
-  tomorrow.setDate(today.getDate() + 1); 
+  const malaysiaTimezoneOffset = 8 * 60; // UTC+8 in minutes
+  const currentUTC = new Date();
+  const malaysiaTime = new Date(currentUTC.getTime() + (malaysiaTimezoneOffset * 60 * 1000));
+
+  malaysiaTime.setHours(0, 0, 0, 0);
+
+  const startOfDayMYT = new Date(malaysiaTime);
+  const endOfDayMYT = new Date(startOfDayMYT);
+  endOfDayMYT.setDate(startOfDayMYT.getDate() + 1);
+
+  const startOfDayUTC = new Date(startOfDayMYT.getTime() - (malaysiaTimezoneOffset * 60 * 1000));
+  const endOfDayUTC = new Date(endOfDayMYT.getTime() - (malaysiaTimezoneOffset * 60 * 1000));
 
   const alerts = await getAlerts({
     where: {
       createdAt: {
-        gte: today, 
-        lt: tomorrow,  
+        gte: startOfDayUTC,
+        lt: endOfDayUTC,
       },
     },
     orderBy: {
@@ -22,6 +30,10 @@ export async function load() {
 
   return { alerts };
 }
+
+
+
+
 
 export const actions: Actions = {
     toggleOne: async ({request}) => {
