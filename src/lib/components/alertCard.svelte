8a2cd2cc6fb6d49396e46ea7f8ac4e04a8b87f81
Amirul Ishak <70368867+amirulishak093@users.noreply.github.com>
2023-11-20 15:53:13 +0800

    Update alertCard.svelte

--- src/lib/components/alertCard.svelte
+++ src/lib/components/alertCard.svelte
@@ -16,7 +16,7 @@
 
 	<div class="py-4 w-full">
 		<div class="flex items-center justify-between">
-			<div class=flex items-center gap-1">
+			<div class="flex items-center gap-1">
 				{#if level === 0}
 					<Icon src={InformationCircle} class='text-info' size="16" />
 				{:else if level === 1}
