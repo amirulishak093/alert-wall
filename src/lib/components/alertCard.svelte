0d3509423a6adeb7b11b9402932cc547e888f42f
Amirul Ishak <70368867+amirulishak093@users.noreply.github.com>
2023-11-22 10:36:41 +0800

    Update alertCard.svelte

--- src/lib/components/alertCard.svelte
+++ src/lib/components/alertCard.svelte
@@ -41,9 +41,9 @@
 		
 		<span class="font-semibold text-sm">{name}</span>
 		{#if !readMore}
-			<div class="flex"><p class="text-sm pt-1">{trimAndEllipsis(description)}</p><a on:click={() => {readMore = !readMore}} class="text-sm link link-primary">Read More</a></div>
+			<div class="flex items-center"><p class="text-sm pt-1">{trimAndEllipsis(description)}</p><a on:click={() => {readMore = !readMore}} class="ml-1 text-sm link link-primary">Read More</a></div>
 		{:else}
-			<div class="flex"><p class="text-sm pt-1">{description}</p><a on:click={() => {readMore = !readMore}} class="text-sm link link-primary">Read Less</a></div>
+			<div class="flex items-center"><p class="text-sm pt-1">{description}</p><a on:click={() => {readMore = !readMore}} class="ml-1 text-sm link link-primary">Read Less</a></div>
 		{/if}
 
 		
