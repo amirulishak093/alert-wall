88f8bba7abab8b6d2e00bbef8a9cf7cf464bb85b
Amirul Ishak <70368867+amirulishak093@users.noreply.github.com>
2023-11-23 12:52:17 +0800

    Update utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,23 +6,28 @@ export function trimAndEllipsis(text: string) {
     }
   }
 
-export function getDateRange(numOfDays: number) {
-    const malaysiaTimezoneOffset = 8 * 60; // UTC+8 in minutes
-    const currentUTC = new Date();
-    const malaysiaTime = new Date(currentUTC.getTime() + (malaysiaTimezoneOffset * 60 * 1000));
+export function getMalaysiaTime(): Date {
+  const malaysiaTimezoneOffset = 8 * 60; // UTC+8 in minutes
+  const currentUTC = new Date();
+  return new Date(currentUTC.getTime() + (malaysiaTimezoneOffset * 60 * 1000));
+}
+
+export function getDateRange(numOfDays: number): { start: Date, end: Date } {
+  const malaysiaTime = getMalaysiaTime();
   
-    const endOfDayMYT = new Date(malaysiaTime);
-    endOfDayMYT.setHours(23, 59, 59, 999);
+  const endOfDayMYT = new Date(malaysiaTime);
+  endOfDayMYT.setHours(23, 59, 59, 999);
   
-    const startOfDayMYT = new Date(endOfDayMYT);
-    startOfDayMYT.setDate(endOfDayMYT.getDate() - numOfDays);
-    startOfDayMYT.setHours(0, 0, 0, 0);
+  const startOfDayMYT = new Date(endOfDayMYT);
+  startOfDayMYT.setDate(endOfDayMYT.getDate() - numOfDays);
+  startOfDayMYT.setHours(0, 0, 0, 0);
   
-    const startOfDayUTC = new Date(startOfDayMYT.getTime() - (malaysiaTimezoneOffset * 60 * 1000));
-    const endOfDayUTC = new Date(endOfDayMYT.getTime() - (malaysiaTimezoneOffset * 60 * 1000));
+  const malaysiaTimezoneOffset = 8 * 60; // UTC+8 in minutes
+  const startOfDayUTC = new Date(startOfDayMYT.getTime() - (malaysiaTimezoneOffset * 60 * 1000));
+  const endOfDayUTC = new Date(endOfDayMYT.getTime() - (malaysiaTimezoneOffset * 60 * 1000));
   
-    return {
-      start: startOfDayUTC,
-      end: endOfDayUTC,
-    };
-  }
+  return {
+    start: startOfDayUTC,
+    end: endOfDayUTC,
+  };
+}
