6cc09e3d90fd95d248f9fed90676b0074364fc25
Amirul Ishak <70368867+amirulishak093@users.noreply.github.com>
2023-11-23 11:41:16 +0800

    Update alertCard.svelte

--- src/lib/components/alertCard.svelte
+++ src/lib/components/alertCard.svelte
@@ -35,7 +35,7 @@
 					<span class="text-error text-sm">CRITICAL</span>
 				{/if}
 
-				<div class="ml-1 badge badge-outline badge-sm">NEW</div>
+				<div class="ml-1 badge badge-outline badge-xs">NEW</div>
 			</div>
 			<div class="text-xs text-[#82878E]">{timestamp}</div>
 		</div>
