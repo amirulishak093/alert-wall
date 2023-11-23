70990bb5200c2c26f700a51e2740c85f33053c81
Amirul Ishak <70368867+amirulishak093@users.noreply.github.com>
2023-11-23 12:15:12 +0800

    Update alertCard.svelte

--- src/lib/components/alertCard.svelte
+++ src/lib/components/alertCard.svelte
@@ -1,6 +1,6 @@
 <script>
 	import { selectedId } from '$lib/store'
-	import  { trimAndEllipsis } from '$lib/utils'
+	import  { trimAndEllipsis, getDateRange } from '$lib/utils'
 	import { XCircle, InformationCircle, ExclamationTriangle, ArchiveBox, ArchiveBoxArrowDown, Icon } from 'svelte-hero-icons';
 
 	export let id = '';
