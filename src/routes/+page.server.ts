84b458645b1d8920764ec3518398c6242d4762a0
Amirul Ishak <70368867+amirulishak093@users.noreply.github.com>
2023-11-23 12:09:01 +0800

    Update +page.server.ts

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,28 +1,14 @@
 import { archiveAllTodayAlerts, getAlerts, toggleAlertArchiveStatus,  } from "$lib/server/services/alert";
 import type { Actions } from "@sveltejs/kit";
 
-
 export async function load() {
-  const malaysiaTimezoneOffset = 8 * 60; // UTC+8 in minutes
-  const currentUTC = new Date();
-  const malaysiaTime = new Date(currentUTC.getTime() + (malaysiaTimezoneOffset * 60 * 1000));
-
-  malaysiaTime.setHours(0, 0, 0, 0);
-
-  const startOfDayMYT = new Date(malaysiaTime);
-  const endOfDayMYT = new Date(startOfDayMYT);
-  endOfDayMYT.setDate(startOfDayMYT.getDate() + 1);
-
-  startOfDayMYT.setDate(startOfDayMYT.getDate() -7);
-
-  const startOfDayUTC = new Date(startOfDayMYT.getTime() - (malaysiaTimezoneOffset * 60 * 1000));
-  const endOfDayUTC = new Date(endOfDayMYT.getTime() - (malaysiaTimezoneOffset * 60 * 1000));
+  const { start, end } = getDateRange(7);
 
   const alerts = await getAlerts({
     where: {
       createdAt: {
-        gte: startOfDayUTC,
-        lt: endOfDayUTC,
+        gte: start,
+        lt: end,
       },
     },
     orderBy: {
@@ -32,10 +18,6 @@ export async function load() {
 
   return { alerts };
 }
-
-
-
-
 
 export const actions: Actions = {
     toggleOne: async ({request}) => {
