b527b1bc57ee29cdd34726d8c28e59ee29e19077
Amirul Ishak <70368867+amirulishak093@users.noreply.github.com>
2023-11-23 12:27:42 +0800

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -58,7 +58,14 @@
 {#if alerts.length > 0}
 	<div>
 		{#each alerts as alert (alert.id) }
-			<AlertCard id={alert.id} level={alert.level} name={alert.name} description={alert.description} archived={alert.archived} timestamp={alert.createdAt} />
+			<AlertCard id={alert.id} level={alert.level} name={alert.name} description={alert.description} archived={alert.archived} timestamp={new Date(alert.createdAt).toLocaleString('en-MY', {
+				year: 'numeric',
+				month: '2-digit',
+				day: '2-digit',
+				hour: '2-digit',
+				minute: '2-digit',
+				second: '2-digit',
+			  })} />
 		{/each}
 	
 
