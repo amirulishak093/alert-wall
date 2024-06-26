1c3172d28316e7255d4e3022020d98d938fa6f2c
Amirul Ishak <70368867+amirulishak093@users.noreply.github.com>
2024-06-26 20:21:26 +0800

    Update alertCard.svelte

--- src/lib/components/alertCard.svelte
+++ src/lib/components/alertCard.svelte
@@ -1,90 +1,85 @@
 <script>
-  import { selectedId } from '$lib/store'
-  import { trimAndEllipsis, getDateRange, convertToMalaysiaTime } from '$lib/utils'
-  import { XCircle, InformationCircle, ExclamationTriangle, ArchiveBox, ArchiveBoxArrowDown, Icon } from 'svelte-hero-icons';
+	import { selectedId } from '$lib/store'
+	import  { trimAndEllipsis, getDateRange, convertToMalaysiaTime } from '$lib/utils'
+	import { XCircle, InformationCircle, ExclamationTriangle, ArchiveBox, ArchiveBoxArrowDown, Icon } from 'svelte-hero-icons';
 
-  export let id = '';
-  export let level = 0 | 1 | 2;
-  export let archived = false;
-  export let name = ''
-  export let description = '';
-  export let createdAt;
-  export let readMore = false;
+	export let id = '';
+	export let level = 0 | 1 | 2;
+	export let archived = false;
+	export let name = ''
+	export let description = '';
+	export let createdAt;
+	export let readMore = false;
 
-  let descriptionEle;
+	let descriptionEle;
 
-  // This function will be called after the component is mounted
-  function updateDescription() {
-    if (descriptionEle) {
-      descriptionEle.innerHTML = description;
-    }
-  }
+	descriptionEle.innerHTML = description;
 
-  // Reactive statement to update description when readMore changes
-  $: if (readMore) {
-    updateDescription();
-  }
+	function getTimestamp(date) {
+		return new Date(date).toLocaleString('en-MY', {
+				year: 'numeric',
+				month: '2-digit',
+				day: '2-digit',
+				hour: '2-digit',
+				minute: '2-digit',
+				second: '2-digit',
+			  });
+	}
 
-  function getTimestamp(date) {
-    return new Date(date).toLocaleString('en-MY', {
-      year: 'numeric',
-      month: '2-digit',
-      day: '2-digit',
-      hour: '2-digit',
-      minute: '2-digit',
-      second: '2-digit',
-    });
-  }
+	  function isNew(date) {
+	    const { start, end } = getDateRange(1);
+	    const itemTime = new Date(date).getTime();
+	    return itemTime >= start.getTime() && itemTime < end.getTime();
+	  }
 
-  function isNew(date) {
-    const { start, end } = getDateRange(1);
-    const itemTime = new Date(date).getTime();
-    return itemTime >= start.getTime() && itemTime < end.getTime();
-  }
 </script>
 
-<div on:click={() => { readMore = !readMore }} class="flex items-center gap-x-3 border-b hover:bg-base-200 px-4 cursor-pointer">
-  <div class="py-4 w-full">
-    <div class="flex items-center justify-between">
-      <div class="flex items-center gap-1">
-        {#if level === 0}
-          <Icon src={InformationCircle} class='text-info' size="16" />
-        {:else if level === 1}
-          <Icon src={ExclamationTriangle} class="text-warning" size="16" />
-        {:else if level === 2}
-          <Icon src={XCircle} class="text-error" size="16" />
-        {/if}
-  
-        {#if level === 0}
-          <span class="text-info text-sm">INFO</span>
-        {:else if level === 1}
-          <span class="text-warning text-sm">WARNING</span>
-        {:else if level === 2}
-          <span class="text-error text-sm">CRITICAL</span>
-        {/if}
+<div on:click={() => {readMore = !readMore}} class="flex items-center gap-x-3 border-b hover:bg-base-200 px-4 cursor-pointer">
 
-        {#if isNew(createdAt)}
-          <div class="ml-1 badge badge-sm">NEW</div>
-        {/if}
-      </div>
-      <div class="text-xs text-[#82878E]">{getTimestamp(createdAt)}</div>
-    </div>
-  
-    <span class="font-semibold text-sm">{name}</span>
-    {#if !readMore}
-      <p class="text-sm pt-1">{trimAndEllipsis(description)}</p>
-    {:else}
-      <p bind:this={descriptionEle} class="text-sm pt-1 whitespace-pre-wrap"></p>
-    {/if}
-  </div>
 
-  {#if !archived}
-    <button on:click={() => { selectedId.set(id) }} onclick="archive_confirmation_modal.showModal()" class="btn btn-square bg-base-100">
-      <Icon src={ArchiveBox} size="20" />
-    </button>
-  {:else}
-    <button on:click={() => { selectedId.set(id) }} onclick="unarchive_confirmation_modal.showModal()" class="btn btn-square bg-base-100">
-      <Icon src={ArchiveBoxArrowDown} size="20" />
-    </button>
-  {/if}
+	<div class="py-4 w-full">
+		<div class="flex items-center justify-between">
+			<div class="flex items-center gap-1">
+				{#if level === 0}
+					<Icon src={InformationCircle} class='text-info' size="16" />
+				{:else if level === 1}
+					<Icon src={ExclamationTriangle} class="text-warning" size="16" />
+				{:else if level === 2}
+					<Icon src={XCircle} class="text-error" size="16" />
+				{/if}
+	
+				{#if level === 0}
+					<span class="text-info text-sm">INFO</span>
+				{:else if level === 1}
+					<span class="text-warning text-sm">WARNING</span>
+				{:else if level === 2}
+					<span class="text-error text-sm">CRITICAL</span>
+				{/if}
+
+			        {#if isNew(createdAt)}
+			          <div class="ml-1 badge badge-sm">NEW</div>
+			        {/if}
+			</div>
+			<div class="text-xs text-[#82878E]">{getTimestamp(createdAt)}</div>
+		</div>
+
+		
+		<span class="font-semibold text-sm">{name}</span>
+		{#if !readMore}
+			<p class="text-sm pt-1">{trimAndEllipsis(description)}</p>
+		{:else}
+			<p bind:this={descriptionEle} class="text-sm pt-1 whitespace-pre-wrap"></p>
+		{/if}
+
+		
+		
+	</div>
+
+	{#if !archived}
+		<button on:click={() => {$selectedId = id}} onclick="archive_confirmation_modal.showModal()" class="btn btn-square bg-base-100"><Icon src={ArchiveBox} size="20" /></button>
+	
+	{:else }
+		<button on:click={() => {$selectedId = id}} onclick="unarchive_confirmation_modal.showModal()" class="btn btn-square bg-base-100"><Icon src={ArchiveBoxArrowDown} size="20" /></button>
+	{/if}
+
 </div>
